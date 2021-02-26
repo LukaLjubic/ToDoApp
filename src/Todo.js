7157a29cf03d5b2b6ce021047bfe506bbe5a6952
Luka Ljubic <luljubic@gmail.com>
2021-02-26 22:07:21 +0100

    2nd commit

--- src/Todo.js
+++ src/Todo.js
@@ -6,14 +6,13 @@ import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 function Todo(props) {
 
-    const todoTextStyle={
-        fontSize: "1rem"
-    }
+    const today = new Date();
+    const date = today.getDate() + '.' + today.getMonth() + '.' + today.getFullYear() + " " + today.getHours() + ':' + today.getMinutes();
 
     return (
         <List className = "todo__list">
             <ListItem>
-                <ListItemText style={todoTextStyle} className="todo__text" primary = {props.todo.todo} secondary = "Dummy Deadline"/>
+                <ListItemText  className="todo__text" primary = {props.todo.todo} secondary = {date}/>
             </ListItem>
             <DeleteForeverIcon className="todo__delete" onClick = {event => db.collection('todo').doc(props.todo.id).delete()} />
         </List>
